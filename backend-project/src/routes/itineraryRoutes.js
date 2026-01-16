4a06c5cae1c839dd55262fe5f9fc58da6bd0a267
Báez Jaramillo Gabriel Alejandro <132725537+GabrielBaezJ@users.noreply.github.com>
2026-01-16 01:35:39 -0500

    Fix: Change protect to authenticate middleware in itineraryRoutes

--- backend-project/src/routes/itineraryRoutes.js
+++ backend-project/src/routes/itineraryRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const itineraryController = require('../controllers/itineraryController');
-const { protect } = require('../middlewares/auth');
+const { authenticate } = require('../middlewares/auth');
 
 /**
  * Itinerary Routes
@@ -9,18 +9,18 @@ const { protect } = require('../middlewares/auth');
  */
 
 // Generate new itinerary
-router.post('/generate', protect, itineraryController.generateItinerary);
+router.post('/generate', authenticate, itineraryController.generateItinerary);
 
 // Get all itineraries for current user
-router.get('/', protect, itineraryController.getUserItineraries);
+router.get('/', authenticate, itineraryController.getUserItineraries);
 
 // Get itinerary by ID
-router.get('/:id', protect, itineraryController.getItineraryById);
+router.get('/:id', authenticate, itineraryController.getItineraryById);
 
 // Get itinerary by trip ID
-router.get('/trip/:tripId', protect, itineraryController.getItineraryByTripId);
+router.get('/trip/:tripId', authenticate, itineraryController.getItineraryByTripId);
 
 // Delete itinerary
-router.delete('/:id', protect, itineraryController.deleteItinerary);
+router.delete('/:id', authenticate, itineraryController.deleteItinerary);
 
 module.exports = router;
