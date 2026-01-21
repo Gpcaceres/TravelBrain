1d64d5a5b696f00071545c20a72abe7403561044
Gpcaceres <gpcaceres@espe.edu.ec>
2026-01-20 23:03:06 -0500

    fixes-menu

--- frontend-react/src/pages/Weather.jsx
+++ frontend-react/src/pages/Weather.jsx
@@ -240,9 +240,9 @@ export default function Weather() {
           <div className="navbar-center">
             <Link to="/dashboard" className="nav-link">Dashboard</Link>
             <Link to="/trips" className="nav-link">My Trips</Link>
+            <Link to="/itineraries" className="nav-link">Itineraries</Link>
             <Link to="/destinations" className="nav-link">Destinations</Link>
             <Link to="/weather" className="nav-link active">Weather</Link>
-            <Link to="/itineraries" className="nav-link">Itineraries</Link>
           </div>
 
           <div className="navbar-right">
