dee9d2a8a7e896e2c91a3f7f2a24093a0027be6d
Gpcaceres <gpcaceres@espe.edu.ec>
2026-01-29 21:49:05 -0500

    fixes

--- frontend-react/src/services/businessRulesService.js
+++ frontend-react/src/services/businessRulesService.js
@@ -4,7 +4,7 @@ import api from './api'
 export const businessRulesService = {
   validateDestinationCreation: async (destinationData) => {
     // Usar la IP pública del backend de reglas de negocio
-    const url = 'http://35.239.79.6:3005/api/destinations/validate-creation';
+    const url = 'http://35.239.79.6:3005/api/business-rules/destinations/validate-creation';
     const response = await api.post(url, destinationData);
     return response.data;
   }
