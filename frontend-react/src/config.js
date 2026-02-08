74c4959d548fa4926c64449bb7fccaaabfba653a
Gpcaceres <gpcaceres@espe.edu.ec>
2026-02-08 00:14:35 -0500

    https-request-front-comunication

--- frontend-react/src/config.js
+++ frontend-react/src/config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: 'http://35.239.79.6:3004',
+  BASE_URL: '',  // Use relative paths - Nginx will proxy to backend
   ENDPOINTS: {
     // Auth
     LOGIN: '/api/auth/login',
