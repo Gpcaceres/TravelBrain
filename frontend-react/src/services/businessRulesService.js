2e8a2a35c2663a557337688635451b3ba01bf548
Gpcaceres <gpcaceres@espe.edu.ec>
2026-02-08 02:20:20 -0500

    fix business rules

--- frontend-react/src/services/businessRulesService.js
+++ frontend-react/src/services/businessRulesService.js
@@ -4,7 +4,7 @@ import api from './api'
 export const businessRulesService = {
   validateDestinationCreation: async (destinationData) => {
     // Use relative path - Nginx will proxy to business-rules backend
-    const url = '/api/business-rules/destinations/validate-creation';
+    const url = '/business-rules/destinations/validate-creation';
     const response = await api.post(url, destinationData);
     return response.data;
   }
