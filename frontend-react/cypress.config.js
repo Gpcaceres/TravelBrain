849d4418cb4331f8b7b2050c33c3208322e27420
Gpcaceres <gpcaceres@espe.edu.ec>
2026-02-11 20:56:48 -0500

    test

--- frontend-react/cypress.config.js
+++ frontend-react/cypress.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: 'https://travelbrain.ddns.net',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots',
+    video: true,
+    screenshotOnRunFailure: true,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config;
     },
   },
 });
